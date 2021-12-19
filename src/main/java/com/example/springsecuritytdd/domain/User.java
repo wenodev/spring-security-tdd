ed1e323642700412d439e628b40dd728f6fc25c4
wenodev <logicalstark1105@gmail.com>
2021-12-20 00:40:44 +0900

    feat: make user entity

--- src/main/java/com/example/springsecuritytdd/domain/User.java
+++ src/main/java/com/example/springsecuritytdd/domain/User.java
@@ -1,13 +1,29 @@
 package com.example.springsecuritytdd.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
     private String password;
+
+    public User() {
+    }
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getEmail() {
--- src/main/java/com/example/springsecuritytdd/infra/UserRepository.java
+++ src/main/java/com/example/springsecuritytdd/infra/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.springsecuritytdd.infra;
 
-public interface UserRepository {
+import com.example.springsecuritytdd.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
