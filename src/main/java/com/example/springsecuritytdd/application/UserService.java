82453e6cc6c9731290b49fb3eb55a6bfff5562c8
wenodev <logicalstark1105@gmail.com>
2021-12-20 00:52:28 +0900

    feat: add findAll in getUsers

--- src/main/java/com/example/springsecuritytdd/application/UserService.java
+++ src/main/java/com/example/springsecuritytdd/application/UserService.java
@@ -1,11 +1,19 @@
 package com.example.springsecuritytdd.application;
 
 import com.example.springsecuritytdd.domain.User;
+import com.example.springsecuritytdd.infra.UserRepository;
 
 import java.util.List;
 
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public List<User> getUsers() {
-        return null;
+        return userRepository.findAll();
     }
 }
--- src/test/java/com/example/springsecuritytdd/application/UserServiceTest.java
+++ src/test/java/com/example/springsecuritytdd/application/UserServiceTest.java
@@ -8,14 +8,27 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class UserServiceTest {
-    UserRepository userRepository;
-    UserService userService = new UserService();
+    UserRepository userRepository = mock(UserRepository.class);
+    UserService userService = new UserService(userRepository);
+
+    private final String EMAIL = "juwon@weno.com";
+    private final String PASSWORD = "1234";
 
     @Test
     void getUsers_메소드는_모든_회원을_리턴한다(){
+        // 준비
+        User user = new User(1L, EMAIL, PASSWORD);
+        given(userRepository.findAll()).willReturn(List.of(user));
+
+        // 실행
         List<User> users = userService.getUsers();
-        assertThat(users.size()).isEqualTo(2);
+
+        // 검증
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users.get(0).getEmail()).isEqualTo(EMAIL);
     }
 }
