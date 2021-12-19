8fc8147f26d42e8eb117a14f45a24dd0773568dd
wenodev <logicalstark1105@gmail.com>
2021-12-20 00:04:11 +0900

    feat: create getUsers

--- src/main/java/com/example/springsecuritytdd/application/UserService.java
+++ src/main/java/com/example/springsecuritytdd/application/UserService.java
@@ -1,4 +1,11 @@
 package com.example.springsecuritytdd.application;
 
+import com.example.springsecuritytdd.domain.User;
+
+import java.util.List;
+
 public class UserService {
+    public List<User> getUsers() {
+        return null;
+    }
 }
--- src/test/java/com/example/springsecuritytdd/application/UserServiceTest.java
+++ src/test/java/com/example/springsecuritytdd/application/UserServiceTest.java
@@ -1,6 +1,16 @@
 package com.example.springsecuritytdd.application;
 
 
+import com.example.springsecuritytdd.domain.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class UserServiceTest {
     UserService userService = new UserService();
+
+    @Test
+    void getUsers_메소드는_모든_회원을_리턴한다(){
+        List<User> users = userService.getUsers();
+    }
 }
