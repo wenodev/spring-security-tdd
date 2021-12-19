8645f6717a64a2be64c97cae294d065431898288
wenodev <logicalstark1105@gmail.com>
2021-12-20 08:18:39 +0900

    chore: add jwt secret

--- src/main/java/com/example/springsecuritytdd/security/TokenProvider.java
+++ src/main/java/com/example/springsecuritytdd/security/TokenProvider.java
@@ -1,13 +1,18 @@
 package com.example.springsecuritytdd.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.security.Key;
 
 public class TokenProvider {
 
-    private Key key;
+    private final Key key;
 
-    public TokenProvider(String secretKey) {
-
+    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyByte = Decoders.BASE64.decode(secretKey);
+        key = Keys.hmacShaKeyFor(keyByte);
     }
 
     public Key getKey() {
