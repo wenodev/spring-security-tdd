c88f56b994f82ae83f746fa3302bcf7f98d3265a
wenodev <logicalstark1105@gmail.com>
2021-12-19 23:53:43 +0900

    test: test fail for getEmail

--- src/test/java/com/example/springsecuritytdd/UserTest.java
+++ src/test/java/com/example/springsecuritytdd/UserTest.java
@@ -1,5 +1,15 @@
 package com.example.springsecuritytdd;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserTest {
     User user = new User();
+
+    @Test
+    void 유저는_이메일과_비밀번호가_있어야한다(){
+        assertThat(user.getEmail()).isEqualTo("juwon@weno.com");
+    }
+
 }
