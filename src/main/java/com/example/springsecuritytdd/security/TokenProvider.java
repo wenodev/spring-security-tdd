5bf26bfbc52edceaffd6a876fafd153ac3cf31c2
wenodev <logicalstark1105@gmail.com>
2021-12-20 01:36:03 +0900

    feat: add TokenProvider

---
+++ src/main/java/com/example/springsecuritytdd/security/TokenProvider.java
@@ -0,0 +1,4 @@
+package com.example.springsecuritytdd.security;
+
+public class TokenProvider {
+}
---
+++ src/test/java/com/example/springsecuritytdd/security/TokenProviderTest.java
@@ -0,0 +1,5 @@
+package com.example.springsecuritytdd.security;
+
+public class TokenProviderTest {
+    TokenProvider tokenProvider = new TokenProvider();
+}
