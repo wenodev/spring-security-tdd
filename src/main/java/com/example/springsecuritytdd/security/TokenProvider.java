3ea5eb534859fca33beb46e1504e9413dea2d5ee
wenodev <logicalstark1105@gmail.com>
2021-12-20 01:46:32 +0900

    test: test construcotr

--- src/main/java/com/example/springsecuritytdd/security/TokenProvider.java
+++ src/main/java/com/example/springsecuritytdd/security/TokenProvider.java
@@ -1,7 +1,16 @@
 package com.example.springsecuritytdd.security;
 
+import java.security.Key;
+
 public class TokenProvider {
+
+    private Key key;
+
     public TokenProvider(String secretKey) {
 
+    }
+
+    public Key getKey() {
+        return key;
     }
 }
--- src/test/java/com/example/springsecuritytdd/security/TokenProviderTest.java
+++ src/test/java/com/example/springsecuritytdd/security/TokenProviderTest.java
@@ -1,15 +1,25 @@
 package com.example.springsecuritytdd.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
+
+import java.security.Key;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TokenProviderTest {
 
-    String secretKey = "weno-secret";
+    String secretKey = "wenosecretwenosecretwenosecretwenosecretwenosecretwenosecretwenosecret";
     TokenProvider tokenProvider = new TokenProvider(secretKey);
 
     @Test
     void 생성자는_주어진_secretKey가_있다면_Key를_만들어준다(){
+        // 준비
+        byte[] keyByte = Decoders.BASE64.decode(secretKey);
+        Key key = Keys.hmacShaKeyFor(keyByte);
 
+        assertThat(keyByte.length).isEqualTo(key.getEncoded().length);
     }
 
 }
