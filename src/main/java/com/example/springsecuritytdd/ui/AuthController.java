6d80af624a286491346ae9069cf8abc11cff7d48
wenodev <logicalstark1105@gmail.com>
2021-12-20 11:10:26 +0900

    test: AuthControllerTest

---
+++ src/main/java/com/example/springsecuritytdd/ui/AuthController.java
@@ -0,0 +1,4 @@
+package com.example.springsecuritytdd.ui;
+
+public class AuthController {
+}
--- src/test/java/com/example/springsecuritytdd/ui/AuthControllerTest.java
+++ src/test/java/com/example/springsecuritytdd/ui/AuthControllerTest.java
@@ -1,7 +1,5 @@
 package com.example.springsecuritytdd.ui;
 
-import com.example.springsecuritytdd.application.UserService;
-import com.example.springsecuritytdd.domain.User;
 import com.example.springsecuritytdd.security.JwtAccessDeniedHandler;
 import com.example.springsecuritytdd.security.JwtAuthenticationEntryPoint;
 import com.example.springsecuritytdd.security.TokenProvider;
@@ -12,20 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.List;
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
+@WebMvcTest(AuthController.class)
+public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private UserService userService;
 
     @MockBean
     private TokenProvider tokenProvider;
@@ -36,21 +27,15 @@ public class UserControllerTest {
     @MockBean
     private JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    private final String EMAIL = "juwon@weno.com";
-    private final String PASSWORD = "1234";
-
     @Test
-    void getUsers_메소드는_모든_회원들과_200을_리턴한다() throws Exception {
+    void signup_메소드는_email과_password가_유효하면_가입된_유저정보와_201을_리턴한다() throws Exception {
         // 준비
-        User user = new User(1L, EMAIL, PASSWORD);
-        given(userService.getUsers()).willReturn(List.of(user));
 
         // 실행
-        ResultActions perform = mockMvc.perform(get("/users"));
-        // 검증
-        perform
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("id").value(1));
+        ResultActions perform = mockMvc.perform(post("/signup"));
+
+        // 과정
+        perform.andExpect(status().isCreated());
     }
 
 }
